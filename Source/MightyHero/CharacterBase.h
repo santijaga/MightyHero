2491782742fb58adb9e230257683761deb0446e1
santijaga <silverstarlingstudio@gmail.com>
2023-12-23 13:30:45 +0100

    Character Base Class

---
+++ Source/MightyHero/CharacterBase.h
@@ -0,0 +1,17 @@
+
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "PaperZDCharacter.h"
+#include "CharacterBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MIGHTYHERO_API ACharacterBase : public APaperZDCharacter
+{
+	GENERATED_BODY()
+	
+};
