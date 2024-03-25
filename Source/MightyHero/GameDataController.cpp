45a00324fb029cd36e1810fcaa108b55883f4c72
santijaga <silverstarlingstudio@gmail.com>
2024-03-25 14:18:06 +0100

    New collectable assets

--- Source/MightyHero/GameDataController.cpp
+++ Source/MightyHero/GameDataController.cpp
@@ -100,27 +100,20 @@ void UGameDataController::SanitizeSaveData()
 
 	if (UGameplayStatics::DoesSaveGameExist(TEXT("MightyHeroHighScore"), 0))
 	{
-		UE_LOG(LogTemp, Warning, TEXT("Exists data in MightyHeroHighScore slot"));
-
 		if (UMightyHeroSaveGame* HighScoreGameInstance = Cast<UMightyHeroSaveGame>(UGameplayStatics::LoadGameFromSlot(TEXT("MightyHeroHighScore"), 0)))
 		{
 			HighScoreData = HighScoreGameInstance->HighScore;
 			MainSlotInstance->HighScore = HighScoreData;
 			UGameplayStatics::DeleteGameInSlot(TEXT("MightyHeroHighScore"), 0);
-
 			bWasSanitized = true;
 		}
 	}
 
 	if (UGameplayStatics::DoesSaveGameExist(TEXT("MightyHeroSoundSettings"), 0))
 	{
-		UE_LOG(LogTemp, Warning, TEXT("Exists data in MightyHeroSoundSettings slot"));
-
 		if (UMightyHeroSaveGame* LoadGameInstance = Cast<UMightyHeroSaveGame>(UGameplayStatics::LoadGameFromSlot(TEXT("MightyHeroSoundSettings"), 0)))
 		{
 			bSoundIsEnabledData = LoadGameInstance->bSoundIsEnabled;
-			UE_LOG(LogTemp, Warning, TEXT("%s saved bSoundIsEnabled value"), bSoundIsEnabledData ? TEXT("true") : TEXT("false"));
-
 			MainSlotInstance->bSoundIsEnabled = bSoundIsEnabledData;
 			UGameplayStatics::DeleteGameInSlot(TEXT("MightyHeroSoundSettings"), 0);
 			bWasSanitized = true;
@@ -129,12 +122,9 @@ void UGameDataController::SanitizeSaveData()
 
 	if (UGameplayStatics::DoesSaveGameExist(TEXT("MightyHeroPawnSettings"), 0))
 	{
-		UE_LOG(LogTemp, Warning, TEXT("Exists data in MightyHeroPawnSettings slot"));
-
 		if (UMightyHeroSaveGame* LoadGameInstance = Cast<UMightyHeroSaveGame>(UGameplayStatics::LoadGameFromSlot(TEXT("MightyHeroPawnSettings"), 0)))
 		{
 			PawnClassData = LoadGameInstance->ActiveActorClass;
-			UE_LOG(LogTemp, Warning, TEXT("%s saved pawned class"), *PawnClassData->GetName());
 			MainSlotInstance->ActiveActorClass = PawnClassData;
 			UGameplayStatics::DeleteGameInSlot(TEXT("MightyHeroPawnSettings"), 0);
 			bWasSanitized = true;
@@ -143,12 +133,9 @@ void UGameDataController::SanitizeSaveData()
 
 	if (UGameplayStatics::DoesSaveGameExist(TEXT("MightyHeroCoinsValue"), 0))
 	{
-		UE_LOG(LogTemp, Warning, TEXT("Exists data in MightyHeroCoinsValue slot"));
-
 		if (UMightyHeroSaveGame* LoadGameInstance = Cast<UMightyHeroSaveGame>(UGameplayStatics::LoadGameFromSlot(TEXT("MightyHeroCoinsValue"), 0)))
 		{
 			CoinsData = LoadGameInstance->Coins;
-			UE_LOG(LogTemp, Warning, TEXT("%d saved Coins value"), CoinsData);
 			UGameplayStatics::DeleteGameInSlot(TEXT("MightyHeroCoinsValue"), 0);
 			MainSlotInstance->Coins = CoinsData;
 			bWasSanitized = true;
@@ -157,14 +144,9 @@ void UGameDataController::SanitizeSaveData()
 
 	if (UGameplayStatics::DoesSaveGameExist(TEXT("MightyHeroUnlockedSkins"), 0))
 	{
-		UE_LOG(LogTemp, Warning, TEXT("Exists data in MightyHeroUnlockedSkins slot"));
 		if (UMightyHeroSaveGame* LoadGameInstance = Cast<UMightyHeroSaveGame>(UGameplayStatics::LoadGameFromSlot(TEXT("MightyHeroUnlockedSkins"), 0)))
 		{
 			SkinsData = LoadGameInstance->UnlockedSkins;
-			for (FString skin : SkinsData)
-			{
-				UE_LOG(LogTemp, Warning, TEXT("%s skin unlocked"), *skin);
-			}
 			MainSlotInstance->UnlockedSkins = SkinsData;
 			UGameplayStatics::DeleteGameInSlot(TEXT("MightyHeroUnlockedSkins"), 0);
 			bWasSanitized = true;
@@ -174,16 +156,6 @@ void UGameDataController::SanitizeSaveData()
 	if (bWasSanitized)
 	{
 		SaveToMainSaveSlot(MainSlotInstance);
-		
-		MainSlotInstance = GetMainSaveSlot();
-		UE_LOG(LogTemp, Warning, TEXT("%d saved HighScore value"), MainSlotInstance->HighScore);
-		UE_LOG(LogTemp, Warning, TEXT("%s saved bSoundIsEnabled value"), MainSlotInstance->bSoundIsEnabled ? TEXT("true") : TEXT("false"));
-		UE_LOG(LogTemp, Warning, TEXT("%s saved pawned class"), *MainSlotInstance->ActiveActorClass->GetName());
-		UE_LOG(LogTemp, Warning, TEXT("%d saved Coins value"), MainSlotInstance->Coins);
-		for (FString skin : MainSlotInstance->UnlockedSkins)
-		{
-			UE_LOG(LogTemp, Warning, TEXT("%s skin unlocked"), *skin);
-		}
 	}
 
 }
